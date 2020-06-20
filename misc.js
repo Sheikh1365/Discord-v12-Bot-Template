53c0a37cfa64b63ca2cfdc430b4cebd5e64dda22
Quackers <qasimopwer@gmail.com>
2020-06-21 00:59:25 +0100

    Add files via upload

---
+++ misc.js
@@ -0,0 +1,33 @@
+exports.Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+exports.Emojibet = {
+  a: "\u1F1E6",
+  b: "\u1F1E7",
+  c: "\u1F1E8",
+  d: "\u1F1E9",
+  e: "\u1F1EA",
+  f: "\u1F1EB",
+  g: "\u1F1EC",
+  h: "\u1F1ED",
+  i: "\u1F1EE",
+  j: "\u1F1EF",
+  k: "\u1F1F0",
+  l: "\u1F1F1",
+  m: "\u1F1F2",
+  n: "\u1F1F3",
+  o: "\u1F1F4",
+  p: "\u1F1F5",
+  q: "\u1F1F6",
+  r: "\u1F1F7",
+  s: "\u1F1F8",
+  t: "\u1F1F9",
+  u: "\u1F1FA",
+  v: "\u1F1FB",
+  w: "\u1F1FC",
+  x: "\u1F1FD",
+  y: "\u1F1FE",
+  z: "\u1F1FF",
+}
+exports.Days       = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+exports.DaysAbbrev = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+exports.Nouns      = ["Cat", "Banana", "Iguana", "Hat", "Robot", "Cell", "Music", "Bird", "Painting", "Sound", "Horse", "Fish", "Cow", "Bear", "Nut", "Crayon", "Leaf", "Flame", "Drop", "Sun", "Star", "Sky", "Shoe"]
+exports.Adjectives = ["Cool", "Hot", "Smooth", "Small", "Large", "Big", "Full", "Rough", "Fast", "Slippery", "Short", "Tall", "Strong", "Smart", "Frail"]
